3c37f63850b3ac1da3d62012827f9b0ab5bfd13b
Jacob Manley <jbmanley45@gmail.com>
2022-05-17 09:34:23 -0700

    commit tree bf8346a99ec92fde1908a25f64d25f40341871c1

---
+++ js/index.js
@@ -0,0 +1,6 @@
+//page load- show first 50 monsters with their name, age, descirption
+//form to create a new monster- fields for name, age, description + 'create MOnster' button
+//on button click- new monster will be saved to the api 
+//bottom of list- show button when clicked to load next 50 monsters
+
+document.addEventListener("DOMContentLoaded", )
